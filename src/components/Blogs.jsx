bca778581e0999de0af4d727b7c406c5ff4ed761
CEPHARD KIPKEMBOI <58904994+cephard@users.noreply.github.com>
2025-05-26 11:18:56 +0100

    improved services page

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -8,14 +8,14 @@ function Blogs() {
         <div className="grid md:grid-cols-3 py-7 gap-x-5 gap-y-8 my-8 justify-center">
             {blogs.map(blog => (
                 <Link to={`/blogpost/${blog.id}`}>
-                    <div key={blog.id} className='card max-w-sm border border-gray-200 rounded overflow-hidden  transition-all duration-300 ease-in-out hover:border-blue-300 hover:shadow-lg'>
+                    <div key={blog.id} className='card border border-gray-300 shadow-gray-300 shadow-xs rounded overflow-hidden h-full  transition-all duration-300 ease-in-out hover:border-blue-300 hover:shadow-md hover:shadow-blue-300'>
                         <img src={`./${blog.image}`} alt={blog.title} className='w-full' />
                         <div className='px-6 py-4'>
                             <h1 className='font-bold text-xl mb-4 text-gray-600'>{blog.title}</h1>
                             <p className='text-gray-500'>{blog.description}</p>
                             <div className='pt-6'>
                                 {blog.tags.map((tag, index) => (
-                                    <span key={index} className="inline-block bg-violet-200 rounded-full px-3 py-1 text-sm font-extralight text-blue-800 mr-2 mb-2 tracking-wider">
+                                    <span key={index} className="inline-block bg-violet-200 rounded-full px-3 py-1 text-xs font-extralight text-blue-800 mr-2 mb-2 tracking-wider">
                                         {tag}
                                     </span>
                                 ))}
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheck } from "@fortawesome/free-solid-svg-icons"
+import { faCheck, faSearch, faLightbulb, faClock, faCode } from "@fortawesome/free-solid-svg-icons"
 
 
 {/**rememeber to overlay the text on the image for small screens */ }
@@ -7,20 +7,24 @@ import { faCheck } from "@fortawesome/free-solid-svg-icons"
 
 function Services() {
     return (
-        <div className="grid lg:grid-cols-2 w-full">
-            <div className="my-4">
+        <div className="grid jus lg:grid-cols-2 w-full py-4">
+            <div className="my-4 w-full grid ">
                 <h1 className="blog-heading">Web Development</h1>
-                <ul className="space-y-5 md:text-xl text-zinc-600">
-                    <li><FontAwesomeIcon icon={faCheck} className="ml-4 mr-2 " /> Search Engine Optimisation</li>
-                    <li><FontAwesomeIcon icon={faCheck} className="ml-4 mr-2 " /> Lightweight</li>
-                    <li><FontAwesomeIcon icon={faCheck} className="ml-4 mr-2 " /> Modern</li>
-                    <li><FontAwesomeIcon icon={faCheck} className="ml-4 mr-2 " /> Minimalist</li>
-                    <li className="inline-flex items-center justify-center rounded-full border hover:border-violet-600 px-4 py-1 my-3 bg-violet-600 text-zinc-50 transition-colors duration-200 hover:bg-transparent hover:text-violet-600 cursor-pointer">
-                        Start Now
-                    </li>
+                <ul className="space-y-5 md:text-xl text-zinc-600 grid grid-cols-2 gap-4">
+                    <li><FontAwesomeIcon icon={faSearch} className="" /> Search Engine Optimisation</li>
+                    <li><FontAwesomeIcon icon={faLightbulb} className="" /> Lightweight</li>
+                    <li><FontAwesomeIcon icon={faClock} className="" /> Modern</li>
+                    <li><FontAwesomeIcon icon={faCode} className="" /> Minimalist</li>
+
                 </ul>
+                <div>
+                    <button className="inline-flex items-center justify-center rounded-full border hover:border-violet-600 px-4 py-1 my-3 bg-violet-600 text-zinc-50 transition-colors duration-200 hover:bg-transparent hover:text-violet-600 cursor-pointer">
+                        Start Now
+                    </button>
+                </div>
+
             </div>
-            <div className="mb-4"><img src="/3431856.jpg" alt="web development" className="rounded" /></div>
+            <div className="mb-4"><img src="./3431856.jpg" alt="web development" className="rounded shadow-gray-400 shadow-md" /></div>
         </div>
     )
 }
